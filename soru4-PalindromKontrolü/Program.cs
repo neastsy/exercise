1941ca0cd82d5cc8664f4abae593a1a240cc089b
NEASTsy <srknyrlmz14@gmail.com>
2025-05-27 19:10:51 +0300

    soru4 için cs dosyası oluşturma ve soruyu yazma

---
+++ soru4-PalindromKontrolü/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Kullanıcıdan bir kelime veya cümle girmesini isteyin. Girdiği metnin bir palindrom olup olmadığını kontrol eden bir C# konsol uygulaması yazın.
+
+Palindrom Nedir?
+Palindrom, düzden okunduğunda da tersten okunduğunda da aynı olan kelime veya cümlelere denir. Örneğin: "kabak", "madam", "ey edip adanada pide ye". (Cümleler kontrol edilirken boşluklar ve noktalama işaretleri göz ardı edilmeli, büyük/küçük harf duyarlılığı kaldırılmalıdır.)
+
+Beklenen Çıktı Örnekleri:
+
+Senaryo 1:
+Lütfen bir kelime veya cümle girin: kabak
+'kabak' bir palindromdur.
+Senaryo 2:
+Lütfen bir kelime veya cümle girin: bilgisayar
+'bilgisayar' bir palindrom değildir.
+Senaryo 3:
+Lütfen bir kelime veya cümle girin: Ey edip adanada pide ye
+'Ey edip adanada pide ye' bir palindromdur.
+Senaryo 4:
+Lütfen bir kelime veya cümle girin:
+Lütfen boş bir metin girmeyin!
+Bu soruda, aşağıdaki konuları kullanmanı bekliyorum:
+
+Konsol Giriş/Çıkış (Console.WriteLine, Console.ReadLine)
+Temel Veri Tipleri (string, bool) ve Değişkenler
+Metotlar (Özellikle palindrom kontrolünü yapan ayrı bir metot yazmalısın.)
+Döngüler (Metni kontrol ederken kullanabilirsin)
+Dizeler (Strings) ve Metin İşlemleri (ToLower(), Replace(), karakterlere erişim, vb.)
+Karar Yapıları (if-else)
+*/
