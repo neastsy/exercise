30e52524f1866e3f4b2ab822276fb7f9b7eced4d
NEASTsy <srknyrlmz14@gmail.com>
2025-05-29 16:18:24 +0300

    sorulan soru yazıldı

---
+++ soru6_TemelHesapMakinesi/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Soru 1: Temel Hesap Makinesi
+
+Kullanıcıdan iki tam sayı alıp bu sayılar üzerinde toplama, çıkarma, çarpma ve bölme işlemlerini gerçekleştiren basit bir hesap makinesi programı yazın. 
+Kullanıcıya hangi işlemi yapmak istediğini sorun ve seçtiği işlemi gerçekleştirip sonucu konsola yazdırın. 
+Bölme işleminde ikinci sayının sıfır olma durumunu kontrol edin ve kullanıcıya uygun bir mesaj gösterin.
+
+*/
+
