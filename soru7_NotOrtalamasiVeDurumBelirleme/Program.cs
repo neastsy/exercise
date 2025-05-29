fd99419d6ce37c114fe55cd7babfe64e5e3a6693
NEASTsy <srknyrlmz14@gmail.com>
2025-05-29 17:22:16 +0300

    soru 7 yazıldı

---
+++ soru7_NotOrtalamasiVeDurumBelirleme/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Harika! O zaman Aşama 1 konularınızı pekiştirmeye devam edelim. İkinci sorumuz geliyor:
+
+Soru 2: Not Ortalaması ve Durum Belirleme
+Bir öğrencinin 3 farklı dersten aldığı notları (0-100 arası tam sayılar) ve bu derslerin kredilerini (1-5 arası tam sayılar) kullanıcıdan alın. Bu notlar ve kredilerle öğrencinin ağırlıklı not ortalamasını hesaplayın.
+
+Ortalama hesaplandıktan sonra, öğrencinin durumunu aşağıdaki kurallara göre belirleyip konsola yazdırın:
+
+Ortalama 85 ve üzeri ise: "Takdir Belgesi"
+Ortalama 70-84 arası ise: "Teşekkür Belgesi"
+Ortalama 50-69 arası ise: "Geçti"
+Ortalama 0-49 arası ise: "Kaldı"
+
+*/
