6fbc954f9f9d1af74065c56eec7273a891aa458c
NEASTsy <srknyrlmz14@gmail.com>
2025-06-03 14:38:22 +0300

    soru9 yazıldı

---
+++ soru9_MetinAnalizAracı/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Soru 9: Metin Analiz Aracı
+Kullanıcıdan bir metin (cümle) girmesini isteyin. Girdiği metin üzerinde aşağıdaki analizleri yapıp sonuçları konsola yazdıran bir program yazın:
+
+Metindeki toplam karakter sayısı. (Boşluklar dahil)
+Metindeki kelime sayısı. (Kelime araları tek boşluk varsayılsın ve noktalama işaretleri kelimeye dahil edilmesin.)
+Metnin tüm harflerini küçük harfe çevirerek yazdırın.
+Metindeki belirli bir karakterin (kullanıcıdan alınacak) kaç kez geçtiğini bulun ve yazdırın. (Büyük/küçük harf duyarlılığı olmadan arama yapın.)
+Metinde belirli bir kelimenin (kullanıcıdan alınacak) bulunup bulunmadığını kontrol edin ve sonucunu yazdırın. (Büyük/küçük harf duyarlılığı olmadan arama yapın.)
+*/
+
